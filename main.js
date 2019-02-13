982c22bab94fc8713a575ae5f5310fb238610469
Sanne <sannevanzeijl@hotmail.com>
2019-02-13 23:43:44 +0100

    duidelijke boxes, opmaak, brandstof meter aangepast

--- main.js
+++ main.js
@@ -7,12 +7,13 @@ var bar = new ProgressBar.Line(container, {
   trailColor: '#eee',
   trailWidth: 1,
   svgStyle: {width: '100%', height: '100%'},
-  from: {color: '#FFEA82'},
-  to: {color: '#ED6A5A'},
+  from: {color: '#FC9303'},
+  to: {color: '#7CE319'},
   step: (state, bar) => {
     bar.path.setAttribute('stroke', state.color);
   }
 });
 
 bar.set(1);
-bar.animate(0);  // Number from 0.0 to 1.0
+bar.animate(0.4);  // Number from 0.0 to 1.0
+
