2bbcf5715b11dd2732885b84ed50e59c8e3051b7
Sanne <sannevanzeijl@hotmail.com>
2019-02-18 11:41:26 +0100

    kleine aanpassingen

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var bar = new ProgressBar.Line(container, {
   duration: 14000,
   color: '#FFEA82',
   trailColor: '#eee',
-  trailWidth: 1,
+  trailWidth: 2,
   svgStyle: {width: '100%', height: '100%'},
   from: {color: '#FC9303'},
   to: {color: '#7CE319'},
@@ -16,4 +16,5 @@ var bar = new ProgressBar.Line(container, {
 
 bar.set(1);
 bar.animate(0.4);  // Number from 0.0 to 1.0
+
 
